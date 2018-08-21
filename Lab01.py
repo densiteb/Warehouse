5de8ba1aa7071260d166a0ec044f6eb216b2d755
densiteb <krittin13@gmail.com>
2018-08-21 23:53:46 +0700

    Lab 02

--- Lab01.py
+++ Lab01.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime as dt
 from datetime import timedelta
 import time as t
@@ -30,3 +29,24 @@ class Warehouse:
             r += 1
         return length,self.row
 
+
+class Product:
+    
+    def __init__(self,name,crtdate = str(dt.now),expdate = str(dt.now() + timedelta(days=730))):
+        self.name = name
+        self.crtdate = crtdate
+        self.expdate = expdate
+        print(self.name)
+        print(self.crtdate.strftime("%Y/%d/%m"))
+
+    def isExpired(self):
+        if dt.now == self.expdate:
+            return True
+        else:
+            return False
+
+    def iteminfo(self):
+        return [self.name,str(self.crtdate.strftime("%Y/%d/%m")),str(self.expdate.strftime("%Y,%d,%m"))]
+
+gh=Product("Milk")
+print(str(gh.iteminfo))
